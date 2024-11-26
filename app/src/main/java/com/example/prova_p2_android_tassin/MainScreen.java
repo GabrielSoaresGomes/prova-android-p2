e2bc68fc5530aacf2f31e40431eee5bfd073e42f
SoSo <gabrielsoares221@gmail.com>
2024-11-25 21:46:49 -0300

    Finalizando

--- app/src/main/java/com/example/prova_p2_android_tassin/MainScreen.java
+++ app/src/main/java/com/example/prova_p2_android_tassin/MainScreen.java
@@ -23,7 +23,7 @@ public class MainScreen extends Activity {
     private ProductAdapter adapter;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
 
--- app/src/main/java/com/example/prova_p2_android_tassin/ProductAdapter.java
+++ app/src/main/java/com/example/prova_p2_android_tassin/ProductAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -43,6 +44,7 @@ public class ProductAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        try {
         if (convertView == null) {
             convertView = LayoutInflater.from(context).inflate(R.layout.product_item, parent, false);
         }
@@ -56,10 +58,14 @@ public class ProductAdapter extends BaseAdapter {
 
         nameView.setText(product.getName());
         priceView.setText(product.getPrice());
-        imageView.setImageBitmap(getBitmapFromURL(product.getImageUrl()));
+        Log.d("Teste Teste", product.getImageUrl());
+        URL newurl = new URL(product.getImageUrl());
+        Bitmap productImageBitMap = BitmapFactory.decodeStream(newurl.openConnection() .getInputStream());
+        imageView.setImageBitmap(productImageBitMap);
         ratingView.setText(product.getRating());
 
         convertView.setOnClickListener(v -> {
+            try {
             Intent intent = new Intent(context, DetailScreen.class);
             intent.putExtra("name", product.getName());
             intent.putExtra("price", product.getPrice());
@@ -67,8 +73,13 @@ public class ProductAdapter extends BaseAdapter {
             intent.putExtra("rating", product.getRating());
             intent.putExtra("description", product.getDescription());
             context.startActivity(intent);
+            } catch (Exception error) {
+                Log.e("ProductAdapter", "Erro", error);
+            }
         });
-
+        } catch (Exception error) {
+            Log.e("ProductAdapter", " Erro Geral", error);
+        }
         return convertView;
     }
 
@@ -81,8 +92,8 @@ public class ProductAdapter extends BaseAdapter {
             InputStream input = connection.getInputStream();
             Bitmap myBitmap = BitmapFactory.decodeStream(input);
             return myBitmap;
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            Log.e("ProductAdapter", "Errinhos", e);
             return null;
         }
     }
